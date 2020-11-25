53b62e9126d54978e92a72d87721de29e4992a45
Kel0 <rozovdima123@gmail.com>
2020-11-25 17:33:21 +0600

    #1 Add Likes and dislikes create feature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,7 +34,29 @@
             method: "POST",
             url: "/phorum/post.comment.create",
             headers: {"X-CSRF-TOKEN": "{{ csrf_token() }}"},
-            data: {"post_id": 1, "content": "Some content"}
+            data: {"post_id": 5, "content": "Some content"}
+        })
+        .then(resp => console.log(resp))
+        .catch(err => console.error(err));
+    }
+
+    const create_post_like_dislike = async () => {
+        await $.ajax({
+            method: "POST",
+            url: "/phorum/post.like_dislike.create",
+            headers: {"X-CSRF-TOKEN": "{{ csrf_token() }}"},
+            data: {"post_id": 5, "action_type": "like"}
+        })
+        .then(resp => console.log(resp))
+        .catch(err => console.error(err));
+    }
+
+    const create_post_comments_like_dislike = async () => {
+        await $.ajax({
+            method: "POST",
+            url: "/phorum/post.comment.like_dislike.create",
+            headers: {"X-CSRF-TOKEN": "{{ csrf_token() }}"},
+            data: {"post_comment_id": 2, "action_type": "like"}
         })
         .then(resp => console.log(resp))
         .catch(err => console.error(err));
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,6 @@ Route::get('/phorum', 'PostsController@index')->name('phorum');
 Route::get('/phorum/posts.get', 'PostsController@posts_get')->name('posts_get');
 
 Route::post('/phorum/post.create', 'PostsController@post_create')->name('post_create');
+Route::post('/phorum/post.like_dislike.create', 'PostsController@post_like_dislike_update')->name('post_like_dislike_update'); 
 Route::post('/phorum/post.comment.create', 'PostsController@post_comment_create')->name('post_comment_create');
+Route::post('/phorum/post.comment.like_dislike.create', 'PostsController@post_comments_like_dislike_update')->name('post_comments_like_dislike_update'); 
